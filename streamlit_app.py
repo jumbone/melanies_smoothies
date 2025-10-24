ad351ff9d548c433b53cb782ec8f074fa5bd5583
jumbone <michael.ferrett@gmail.com>
2025-10-23 17:27:30 -0700

    Typo in connections

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.write(  """Choose the fruits you want in your custom smoothie!
   """
 )
 
-cnx = st.connections("snowflake")
+cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
